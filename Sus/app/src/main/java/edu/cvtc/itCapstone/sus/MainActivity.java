489fe906036d317d9ef262e835d02cb991a27c14
cvtc-Hunter-Bauer <78509635+cvtc-Hunter-Bauer@users.noreply.github.com>
2021-04-22 13:54:35 -0500

    [delivers #177745131] Added a tutorial that only shows on first luanch

--- Sus/app/src/main/java/edu/cvtc/itCapstone/sus/MainActivity.java
+++ Sus/app/src/main/java/edu/cvtc/itCapstone/sus/MainActivity.java
@@ -2,7 +2,9 @@ package edu.cvtc.itCapstone.sus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -11,6 +13,8 @@ import android.widget.Toast;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String MY_PREFS = "MyPrefs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +30,22 @@ public class MainActivity extends AppCompatActivity {
                 //Toast.makeText(MainActivity.this, "Fab", Toast.LENGTH_SHORT).show();
             }
         });
+
+
+        SharedPreferences sp = getSharedPreferences(MY_PREFS, Context.MODE_PRIVATE);
+        if (sp.getBoolean("firstStart", true)) {
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putBoolean("firstStart", false);
+            editor.apply();
+
+            View overlayPageOne = findViewById(R.id.OverlayPageOne);
+            overlayPageOne.setVisibility(View.VISIBLE);
+        }
     }
+
+    public void closeTutorial(View view) {
+        View overlayPageOne = findViewById(R.id.OverlayPageOne);
+        overlayPageOne.setVisibility(View.INVISIBLE);
+    }
+
 }
