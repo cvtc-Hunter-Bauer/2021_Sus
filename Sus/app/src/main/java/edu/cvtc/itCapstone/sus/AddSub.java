f814b52fb3392bcd10ffc8b9e1f23b38ddbf6b2d
Matt Olson <52805964+Bigmo1575@users.noreply.github.com>
2021-05-07 20:08:25 -0500

    Update AddSub.java

--- Sus/app/src/main/java/edu/cvtc/itCapstone/sus/AddSub.java
+++ Sus/app/src/main/java/edu/cvtc/itCapstone/sus/AddSub.java
@@ -85,7 +85,7 @@ public class AddSub extends AppCompatActivity implements LoaderManager.LoaderCal
 
             @Override
             public void onClick(View v) {
-                if (mName.getText().toString().isEmpty() || mDescription.getText().toString().isEmpty() || mCost.getText().toString().isEmpty() || mDate.getText().toString().isEmpty()) {
+                if (mName.getText().toString().trim().isEmpty() || mDescription.getText().toString().trim().isEmpty() || mCost.getText().toString().trim().isEmpty() || mDate.getText().toString().trim().isEmpty()) {
                     Toast.makeText(AddSub.this, "Please make sure the fields are filled", Toast.LENGTH_SHORT).show();
                 } else {
                     if (savedInstanceState == null) {
